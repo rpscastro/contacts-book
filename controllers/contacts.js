94b216ff13e088bc0c07d4f7a99d2d255d7f1e07
Rafael Pereira de Souza Castro <rcastro2@byupathway.edu>
2026-03-20 19:23:42 -0300

    feat: add CRUD operations for contacts and update server configuration

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -26,7 +26,93 @@ const getContactById = async (req, res) => {
   });
 };
 
+const createContact = async (req, res) => {
+  // #swagger.tags = ['Contacts']
+  // Implementation for creating a new contact
+    const contactData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    address: req.body.address,
+    telephoneNumber: req.body.telephoneNumber,
+    birthdate: req.body.birthdate,
+    favoriteColor: req.body.favoriteColor,
+    nationality: req.body.nationality,
+    email: req.body.email,
+    socialMedia: req.body.socialMedia,
+    company: req.body.company
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .insertOne(contactData);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact.",
+      );
+  }
+};
+
+const updateContact = async (req, res) => {
+  // #swagger.tags = ['Contacts']
+  // Implementation for updating an existing contact
+  const contactId = new ObjectId(req.params.id);
+  const contactData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    address: req.body.address,
+    telephoneNumber: req.body.telephoneNumber,
+    birthdate: req.body.birthdate,
+    favoriteColor: req.body.favoriteColor,
+    nationality: req.body.nationality,
+    email: req.body.email,
+    socialMedia: req.body.socialMedia,
+    company: req.body.company,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: contactId }, contactData);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while updating the contact.",
+      );
+  }
+};
+
+const deleteContact = async (req, res) => {
+  // #swagger.tags = ['Contacts']
+  // Implementation for deleting a contact
+  const contactId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .deleteOne({ _id: contactId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact.",
+      );
+  }
+};
+
 module.exports = {
   getAllContacts,
   getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
